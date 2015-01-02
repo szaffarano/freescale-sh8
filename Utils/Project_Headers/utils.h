5817972692789e8b246544ae1989fce2ceddcbeb
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2015-01-02 10:12:09 -0300

    Agrega libreria con funciones utils en asm

---
+++ Utils/Project_Headers/utils.h
@@ -0,0 +1,15 @@
+/*
+ * utils.h
+ *
+ *  Created on: Jan 2, 2015
+ *      Author: sebas
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void toggle_led(void);
+void square_wave(void);
+void uart_echo(void);
+
+#endif /* UTILS_H_ */
